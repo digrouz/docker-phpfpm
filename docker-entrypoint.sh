e092aaed5d285571ffbc6a238810a1cd602807f5
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-09-06 11:38:20 +0000

    fixing service startup command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,7 +21,7 @@ if ! /bin/grep -q "${MYUSER}" /etc/passwd; then
 fi
 
 if [ "$1" = 'php-fpm' ]; then
-    exec /sbin/su-exec "${MYUSER}" /usr/bin/php-fpm --fpm-config /etc/php5/php-fpm.conf -F "$@"
+    exec /sbin/su-exec "${MYUSER}" /usr/bin/php-fpm --fpm-config /etc/php5/php-fpm.conf -c /etc/php5 -F --force-stderr "$@"
 fi
 
 exec "$@"
